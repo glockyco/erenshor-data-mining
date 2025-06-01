75d13feef580200b83e268114799f631f53d17ad
Johann Glock <11704293+glockyco@users.noreply.github.com>
2025-06-01 18:21:14 +0200

    Add more config settings for screenshotter area

--- Assets/Editor/Mapping/TileScreenshotter.cs
+++ Assets/Editor/Mapping/TileScreenshotter.cs
@@ -4,45 +4,61 @@ using UnityEngine.SceneManagement;
 
 public class TileScreenshotter
 {
-    public static bool DryRun = true;
-    
-    public static string OutputRoot = null;
-    
-    public static float CameraHeight = 1000f;
-    public static float AreaWorldSize = 512f;
-    public static int TilePixelSize = 256;
-    public static int BaseTilesPerAxis = 2;
-    public static int ZoomLevels = 3;
+    public class TileShotterSettings
+    {
+        public bool DryRun { get; set; } = true;
+
+        public string OutputRoot { get; set; } = null;
+
+        public float CameraHeight { get; set; } = 1000f;
+        public int ZoomLevels { get; set; } = 3;
+        public int OriginX { get; set; } = 0;
+        public int OriginY { get; set; } = 0;
+        public int BaseTilesX { get; set; } = 2;
+        public int BaseTilesY { get; set; } = 2;
+        public int TilePixelSize { get; set; } = 256;
+        public float TileWorldSize { get; set; } = 256f;
+    }
 
     public static void Run()
     {
+        TileScreenshotter.Run(new TileShotterSettings {});
+    }
+    
+    public static void Run(TileShotterSettings settings)
+    {
         Time.timeScale = 0f;
-        
+
         Camera cam = Camera.main;
         Scene scene = SceneManager.GetActiveScene();
 
-        if (DryRun)
+        if (settings.DryRun)
         {
-            var z = ZoomLevels - 1;
-            var tilesPerAxis = BaseTilesPerAxis << z;
-            var unitsPerTile = AreaWorldSize / tilesPerAxis;
+            var z = settings.ZoomLevels - 1;
+            var tilesX = settings.BaseTilesX << z;
+            var tilesY = settings.BaseTilesY << z;
+            var unitsPerTile = settings.TileWorldSize / (1 << z); // Adjust if you want tile size to decrease with zoom
 
             cam.orthographic = true;
             cam.nearClipPlane = 0.1f;
             cam.farClipPlane = 2000f;
-            cam.transform.position = new Vector3(AreaWorldSize / 2f, CameraHeight, AreaWorldSize / 2f);
-            cam.transform.rotation = Quaternion.Euler(90f, 0f, 0f);
-            cam.orthographicSize = AreaWorldSize / 2f;
 
-            var outlineY = CameraHeight / 2;
+            // Center camera over the tiling area
+            var areaWidth = tilesX * unitsPerTile;
+            var areaHeight = tilesY * unitsPerTile;
+            cam.transform.position = new Vector3(settings.OriginX + areaWidth / 2f, settings.CameraHeight, settings.OriginY + areaHeight / 2f);
+            cam.transform.rotation = Quaternion.Euler(90f, 0f, 0f);
+            cam.orthographicSize = Mathf.Max(areaWidth, areaHeight) / 2f;
+
+            var outlineY = settings.CameraHeight / 2;
             var parent = new GameObject("TileOutlines");
 
-            for (var x = 0; x < tilesPerAxis; x++)
+            for (var x = 0; x < tilesX; x++)
             {
-                for (var y = 0; y < tilesPerAxis; y++)
+                for (var y = 0; y < tilesY; y++)
                 {
-                    var centerX = unitsPerTile * (x + 0.5f);
-                    var centerZ = unitsPerTile * (y + 0.5f);
+                    var centerX = settings.OriginX + unitsPerTile * (x + 0.5f);
+                    var centerZ = settings.OriginY + unitsPerTile * (y + 0.5f);
                     var half = unitsPerTile / 2f;
 
                     var corners = new Vector3[5]
@@ -64,21 +80,21 @@ public class TileScreenshotter
                     lr.startColor = lr.endColor = Color.red;
                     lr.useWorldSpace = true;
 
-                    GameObject textGO = new GameObject($"TileText_{x}_{y}");
-                    textGO.transform.parent = parent.transform;
-                    textGO.transform.position = new Vector3(centerX, outlineY + 5f, centerZ);
+                    GameObject textObject = new GameObject($"TileText_{x}_{y}");
+                    textObject.transform.parent = parent.transform;
+                    textObject.transform.position = new Vector3(centerX, outlineY + 5f, centerZ);
 
                     var leafletX = x;
                     var leafletY = -(y + 1);
 
-                    var textMesh = textGO.AddComponent<TextMesh>();
+                    var textMesh = textObject.AddComponent<TextMesh>();
                     textMesh.text = $"z={z}\n{leafletX},{leafletY}";
                     textMesh.fontSize = 64;
                     textMesh.characterSize = unitsPerTile / 64f;
                     textMesh.anchor = TextAnchor.MiddleCenter;
                     textMesh.alignment = TextAlignment.Center;
                     textMesh.color = Color.white;
-                    textGO.transform.rotation = Quaternion.Euler(90f, 0f, 0f);
+                    textObject.transform.rotation = Quaternion.Euler(90f, 0f, 0f);
                 }
             }
 
@@ -86,39 +102,40 @@ public class TileScreenshotter
         }
         else
         {
-            if (OutputRoot == null)
+            if (settings.OutputRoot == null)
             {
                 Debug.LogError("OutputRoot is null, cannot run screenshotter.");
                 return;
             }
-            
-            for (var z = 0; z < ZoomLevels; z++)
+
+            for (var z = 0; z < settings.ZoomLevels; z++)
             {
-                var tilesPerAxis = BaseTilesPerAxis << z;
-                var unitsPerTile = AreaWorldSize / tilesPerAxis;
+                var tilesX = settings.BaseTilesX << z;
+                var tilesY = settings.BaseTilesY << z;
+                var unitsPerTile = settings.TileWorldSize / (1 << z);
 
                 cam.orthographic = true;
                 cam.nearClipPlane = 0.1f;
                 cam.farClipPlane = 2000f;
 
-                var rt = new RenderTexture(TilePixelSize, TilePixelSize, 24);
-                var tex = new Texture2D(TilePixelSize, TilePixelSize, TextureFormat.RGB24, false);
+                var rt = new RenderTexture(settings.TilePixelSize, settings.TilePixelSize, 24);
+                var tex = new Texture2D(settings.TilePixelSize, settings.TilePixelSize, TextureFormat.RGB24, false);
 
-                for (var x = 0; x < tilesPerAxis; x++)
+                for (var x = 0; x < tilesX; x++)
                 {
-                    for (var y = 0; y < tilesPerAxis; y++)
+                    for (var y = 0; y < tilesY; y++)
                     {
-                        var centerX = unitsPerTile * (x + 0.5f);
-                        var centerZ = unitsPerTile * (y + 0.5f);
+                        var centerX = settings.OriginX + unitsPerTile * (x + 0.5f);
+                        var centerZ = settings.OriginY + unitsPerTile * (y + 0.5f);
 
-                        cam.transform.position = new Vector3(centerX, CameraHeight, centerZ);
+                        cam.transform.position = new Vector3(centerX, settings.CameraHeight, centerZ);
                         cam.transform.rotation = Quaternion.Euler(90f, 0f, 0f);
                         cam.orthographicSize = unitsPerTile / 2f;
 
                         var leafletX = x;
                         var leafletY = -(y + 1);
 
-                        var dir = $"{scene.name}/{OutputRoot}/{z}/{leafletX}";
+                        var dir = $"{scene.name}/{settings.OutputRoot}/{z}/{leafletX}";
                         Directory.CreateDirectory(dir);
                         var path = $"{dir}/{leafletY}.jpg";
 
@@ -132,7 +149,7 @@ public class TileScreenshotter
                         cam.Render();
 
                         RenderTexture.active = rt;
-                        tex.ReadPixels(new Rect(0, 0, TilePixelSize, TilePixelSize), 0, 0);
+                        tex.ReadPixels(new Rect(0, 0, settings.TilePixelSize, settings.TilePixelSize), 0, 0);
                         tex.Apply();
                         var bytes = tex.EncodeToJPG(85);
                         File.WriteAllBytes(path, bytes);
