a515ea19d406ac460b0f85c8b3517a2cf283e953
Johann Glock <11704293+glockyco@users.noreply.github.com>
2025-06-15 11:07:45 +0200

    Add new DB table `Coordinates`
    Will be used to store coordinates of anything that has an actual
    position in the game world (e.g., achievement triggers, characters, ..).

---
+++ Assets/Editor/Database/CoordinateDBRecord.cs
@@ -0,0 +1,34 @@
+﻿using SQLite;
+
+[Table("Coordinates")]
+public class CoordinateDBRecord
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+    
+    public string Scene { get; set; }
+    public float X { get; set; }
+    public float Y { get; set; }
+    public float Z { get; set; }
+    
+    public string Category { get; set; }
+    
+    public int AchievementTriggerId { get; set; }
+    public int CharacterId { get; set; }
+    public int MiningNodeId { get; set; }
+    public int SpawnPointId { get; set; }
+    public int TreasureLocId { get; set; }
+    public int WaterId { get; set; }
+    public int ZoneLineId { get; set; }
+    
+    public enum CoordinateCategory
+    {
+        AchievementTrigger,
+        Character,
+        MiningNode,
+        SpawnPoint,
+        TreasureLoc,
+        Water,
+        ZoneLine,
+    }
+}
