8121214e90d86ec13962c5b7a5b93250a60bd837
Johann Glock <11704293+glockyco@users.noreply.github.com>
2025-06-22 15:08:54 +0200

    Adapt WikiComparisonDBRecord for '#nullable enable'

--- Assets/Editor/Database/WikiComparisonDBRecord.cs
+++ Assets/Editor/Database/WikiComparisonDBRecord.cs
@@ -1,14 +1,16 @@
+#nullable enable
+
 using SQLite;
 
 [Table("WikiComparison")]
 public class WikiComparisonDBRecord
 {
-    public string WikiUrl { get; set;}
-    public string Type { get; set; }
-    public string Name { get; set; }
+    public string WikiUrl { get; set; } = string.Empty;
+    public string Type { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
     public int Tier { get; set; }
-    public string ComparisonResult { get; set; }
-    public string CurrentWikiString { get; set; }
-    public string SuggestedWikiString { get; set; }
-    public string ComparisonTimestamp { get; set; }
+    public string ComparisonResult { get; set; } = string.Empty;
+    public string CurrentWikiString { get; set; } = string.Empty;
+    public string SuggestedWikiString { get; set; } = string.Empty;
+    public string ComparisonTimestamp { get; set; } = string.Empty;
 }
