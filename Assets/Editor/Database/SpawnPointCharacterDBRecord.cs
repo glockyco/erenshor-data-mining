a73e9290f834879b23531721f97b3dcc21dca6be
Johann Glock <11704293+glockyco@users.noreply.github.com>
2025-06-22 15:01:01 +0200

    Adapt SpawnPointCharacterDBRecord for '#nullable enable'

--- Assets/Editor/Database/SpawnPointCharacterDBRecord.cs
+++ Assets/Editor/Database/SpawnPointCharacterDBRecord.cs
@@ -1,3 +1,5 @@
+#nullable enable
+
 using SQLite;
 
 [Table("SpawnPointCharacters")]
@@ -6,7 +8,7 @@ public class SpawnPointCharacterDBRecord
     [Indexed(Name = "SpawnPointCharacters_Primary_IDX", Order = 1, Unique = true)]
     public int SpawnPointId { get; set; }
     [Indexed(Name = "SpawnPointCharacters_Primary_IDX", Order = 2, Unique = true)]
-    public string CharacterPrefabGuid { get; set; }
+    public string CharacterPrefabGuid { get; set; } = string.Empty;
     public float SpawnChance { get; set; }
     public bool IsCommon { get; set; }
     public bool IsRare { get; set; }
