cc459baaa9f9963e0e16f7248db94f0ba0fd7cba
Johann Glock <11704293+glockyco@users.noreply.github.com>
2025-06-22 15:04:47 +0200

    Adapt WaterFishableRecord for '#nullable enable'

--- Assets/Editor/Database/WaterFishableRecord.cs
+++ Assets/Editor/Database/WaterFishableRecord.cs
@@ -1,3 +1,5 @@
+#nullable enable
+
 using SQLite;
 
 [Table("WaterFishables")]
@@ -6,8 +8,8 @@ public class WaterFishableDBRecord
     [Indexed(Name = "WaterFishable_Primary_IDX", Order = 1, Unique = true)]
     public int WaterId { get; set; }
     [Indexed(Name = "WaterFishable_Primary_IDX", Order = 2, Unique = true)]
-    public string Type { get; set; } // "DayFishable" or "NightFishable"
+    public string Type { get; set; } = string.Empty; // "DayFishable" or "NightFishable"
     [Indexed(Name = "WaterFishable_Primary_IDX", Order = 3, Unique = true)]
-    public string ItemName { get; set; }
+    public string ItemName { get; set; } = string.Empty;
     public float DropChance { get; set; }
 }
