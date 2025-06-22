75f2a54d5684c6547d7e85dc2e54a451523da601
Johann Glock <11704293+glockyco@users.noreply.github.com>
2025-06-22 15:03:42 +0200

    Adapt TreasureHuntingDBRecord for '#nullable enable'

--- Assets/Editor/Database/TreasureHuntingDBRecord.cs
+++ Assets/Editor/Database/TreasureHuntingDBRecord.cs
@@ -1,11 +1,13 @@
+#nullable enable
+
 using SQLite;
 
 [Table("TreasureHunting")]
 public class TreasureHuntingDBRecord
 {
     [PrimaryKey]
-    public string ZoneName { get; set; }
-    public string ZoneDisplayName { get; set; }
+    public string ZoneName { get; set; } = string.Empty;
+    public string ZoneDisplayName { get; set; } = string.Empty;
     public bool IsPickableAlways { get; set; }
     public bool IsPickableGreater20 { get; set; }
     public bool IsPickableGreater30 { get; set; }
