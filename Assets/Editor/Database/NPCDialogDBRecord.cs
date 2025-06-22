57bcef039c4b740ef454495f22cf792a8dbc59a5
Johann Glock <11704293+glockyco@users.noreply.github.com>
2025-06-22 14:57:03 +0200

    Adapt NPCDialogDBRecord for '#nullable enable'

--- Assets/Editor/Database/NPCDialogDBRecord.cs
+++ Assets/Editor/Database/NPCDialogDBRecord.cs
@@ -1,3 +1,5 @@
+#nullable enable
+
 using SQLite;
 
 [Table("NPCDialogs")]
@@ -9,13 +11,13 @@ public class NPCDialogDBRecord
     [Indexed(Name = "NPCDialogs_Primary_IDX", Order = 2, Unique = true)]
     public int DialogIndex { get; set; } // Running index for dialogs associated with this NPC
 
-    public string DialogText { get; set; } // NPCDialog.Dialog
-    public string Keywords { get; set; } // NPCDialog.KeywordToActivate (serialized as comma-separated string)
-    public string GiveItemName { get; set; } // NPCDialog.GiveItem?.ItemName
-    public string AssignQuestDBName { get; set; } // NPCDialog.QuestToAssign?.DBName
-    public string CompleteQuestDBName { get; set; } // NPCDialog.QuestToComplete?.DBName
-    public string RepeatingQuestDialog { get; set; } // NPCDialog.RepeatingQuestDialog
+    public string DialogText { get; set; } = string.Empty; // NPCDialog.Dialog
+    public string Keywords { get; set; } = string.Empty; // NPCDialog.KeywordToActivate (serialized as comma-separated string)
+    public string GiveItemName { get; set; } = string.Empty; // NPCDialog.GiveItem?.ItemName
+    public string AssignQuestDBName { get; set; } = string.Empty; // NPCDialog.QuestToAssign?.DBName
+    public string CompleteQuestDBName { get; set; } = string.Empty; // NPCDialog.QuestToComplete?.DBName
+    public string RepeatingQuestDialog { get; set; } = string.Empty; // NPCDialog.RepeatingQuestDialog
     public bool KillSelfOnSay { get; set; } // NPCDialog.KillMeOnSay
-    public string RequiredQuestDBName { get; set; } // NPCDialog.RequireQuestComplete?.DBName
-    public string SpawnName { get; set; } // NPCDialog.Spawn?.name
+    public string RequiredQuestDBName { get; set; } = string.Empty; // NPCDialog.RequireQuestComplete?.DBName
+    public string SpawnName { get; set; } = string.Empty; // NPCDialog.Spawn?.name
 }
