d8d130b790979b1ba858a2463dfb7ccff34a4d69
Johann Glock <11704293+glockyco@users.noreply.github.com>
2025-07-06 11:37:29 +0200

    Add map links to achievement trigger export

--- Assets/Editor/export.sql
+++ Assets/Editor/export.sql
@@ -365,11 +365,13 @@ ORDER BY za.ZoneName;
 -- achievement-triggers
 SELECT
     --Id,
+    za.SceneName,
     za.ZoneName,
     ROUND(c.X, 2) AS PositionX,
     ROUND(c.Y, 2) AS PositionY,
     ROUND(c.Z, 2) AS PositionZ,
-    AchievementName
+    AchievementName,
+    'https://erenshor-maps.wowmuch1.workers.dev/' || za.SceneName || '?coordinateId=' || c.Id AS MapLink
 FROM AchievementTriggers at
 JOIN Coordinates c ON c.AchievementTriggerId = at.Id
 JOIN ZoneAnnounces za ON za.SceneName = c.Scene
