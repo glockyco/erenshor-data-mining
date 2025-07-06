3f256628092b968d093f8fe71aec4d757229219a
Johann Glock <11704293+glockyco@users.noreply.github.com>
2025-07-06 11:22:16 +0200

    Add SQL export for wishing wells

--- Assets/Editor/export.sql
+++ Assets/Editor/export.sql
@@ -248,6 +248,20 @@ JOIN Coordinates co ON co.Scene = th.ZoneName AND co.Category = 'TreasureLoc'
 JOIN ZoneAnnounces za ON za.SceneName = th.ZoneName
 ORDER BY th.IsPickableAlways DESC, th.IsPickableGreater20 DESC, th.IsPickableGreater30;
 
+-- wishing-wells
+SELECT
+    --Id,
+    za.SceneName,
+    za.ZoneName,
+    ROUND(c.X, 2) AS PositionX,
+    ROUND(c.Y, 2) AS PositionY,
+    ROUND(c.Z, 2) AS PositionZ,
+    'https://erenshor-maps.wowmuch1.workers.dev/' || za.SceneName || '?coordinateId=' || c.Id AS MapLink
+FROM Coordinates c
+JOIN ZoneAnnounces za ON za.SceneName = c.Scene
+WHERE Category = 'WishingWell'
+ORDER BY za.ZoneName;
+
 -- fishing
 SELECT * FROM
 (
