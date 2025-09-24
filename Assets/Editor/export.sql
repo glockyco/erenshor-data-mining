20b5b324e1aba04962db4606ac198e4a1e3f08fb
Johann Glock <11704293+glockyco@users.noreply.github.com>
2025-09-24 18:06:48 +0200

    Add SQL export for item bags

--- Assets/Editor/export.sql
+++ Assets/Editor/export.sql
@@ -117,6 +117,20 @@ ORDER BY i.ItemDBIndex,
         ELSE 4 
     END;
 
+-- item-bags
+SELECT
+    ItemId,
+    ItemName,
+    za.ZoneName,
+    ROUND(co.X, 2) AS PositionX,
+    ROUND(co.Y, 2) AS PositionY,
+    ROUND(co.Z, 2) AS PositionZ
+FROM ItemBags ib
+JOIN Items i ON i.Id = ib.ItemId
+JOIN Coordinates co ON co.Id = ib.CoordinateId
+JOIN ZoneAnnounces za ON za.SceneName = co.Scene
+ORDER by Scene, ItemName;
+
 -- characters
 SELECT
    -- Identity & Basic Info
