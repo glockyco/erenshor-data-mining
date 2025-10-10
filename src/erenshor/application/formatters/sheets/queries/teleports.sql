d08ea0875bb75da8f5d9a16f70c7e8b76ecb8d37
Johann Glock <11704293+glockyco@users.noreply.github.com>
2025-10-10 18:11:24 +0200

    refactor: remove redundant teleport-destinations sheet

--- src/erenshor/application/formatters/sheets/queries/teleports.sql
+++ src/erenshor/application/formatters/sheets/queries/teleports.sql
@@ -4,7 +4,8 @@ SELECT
     za.ZoneName,
     ROUND(co.X, 2) AS PositionX,
     ROUND(co.Y, 2) AS PositionY,
-    ROUND(co.Z, 2) AS PositionZ
+    ROUND(co.Z, 2) AS PositionZ,
+    'https://erenshor-maps.wowmuch1.workers.dev/' || za.SceneName || '?coordinateId=' || co.Id AS MapLink
 FROM Teleports t
 JOIN Coordinates co ON co.TeleportId = t.Id
 JOIN ZoneAnnounces za ON za.SceneName = co.Scene
