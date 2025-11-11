5695280c2a792976582b529f0b26836b49fcc1da
Johann Glock <11704293+glockyco@users.noreply.github.com>
2025-11-11 10:53:34 +0100

    fix: update quests.sql to new DB structure
    Quests data is now split across the `Quests` and `QuestVariants` tables.

--- src/erenshor/application/sheets/queries/quests.sql
+++ src/erenshor/application/sheets/queries/quests.sql
@@ -1,1 +1,23 @@
-SELECT * FROM quests;
+SELECT
+    qv.QuestDBIndex,
+    q.StableKey,
+    qv.QuestName,
+    qv.QuestDesc,
+    qv.XPonComplete,
+    qv.ItemOnCompleteStableKey,
+    qv.GoldOnComplete,
+    qv.AssignNewQuestOnCompleteStableKey,
+    qv.DialogOnSuccess,
+    qv.DialogOnPartialSuccess,
+    qv.DisableText,
+    qv.AssignThisQuestOnPartialComplete,
+    qv.Repeatable,
+    qv.DisableQuest,
+    qv.KillTurnInHolder,
+    qv.DestroyTurnInHolder,
+    qv.DropInvulnOnHolder,
+    qv.OncePerSpawnInstance,
+    qv.SetAchievementOnGet,
+    qv.SetAchievementOnFinish
+FROM Quests q
+LEFT JOIN QuestVariants qv ON qv.QuestStableKey = q.StableKey;
