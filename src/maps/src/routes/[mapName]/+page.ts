0a64041423e81cbd458cecd9f3e6a68d67a5ccb2
Johann Glock <11704293+glockyco@users.noreply.github.com>
2025-11-16 21:01:29 +0100

    fix(maps): disable prerendering of maps pages

--- src/maps/src/routes/[mapName]/+page.ts
+++ src/maps/src/routes/[mapName]/+page.ts
@@ -1,5 +1,8 @@
 import type { Load } from '@sveltejs/kit';
 
+// Disable prerendering since we use URL search params (coordinateId) dynamically
+export const prerender = false;
+
 export const load: Load = ({ params }) => {
 	return {
 		mapName: params.mapName
